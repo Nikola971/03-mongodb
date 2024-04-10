affd19e1b427e0606a2f8df3f13395b109683373
Nikola971 <mukola.milyan@outlook.com>
2024-04-10 17:32:41 +0300

    drh

---
+++ controllers/contactsControllers.js
@@ -0,0 +1,50 @@
+import contactsServices from '../services/contactsServices.js';
+import ctrlWrapper from '../decorators/ctrlWrapper.js';
+import HttpError from '../helpers/HttpError.js';
+
+const getAll = async (req, res) => {
+  const result = await contactsServices.listContacts();
+  res.json(result);
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsServices.getContactById(id);
+  if (!result) throw HttpError(404, `Not found`);
+  res.json(result);
+};
+
+const addContact = async (req, res) => {
+  const result = await contactsServices.addContact(req.body);
+  res.status(201).json(result);
+};
+
+const updateContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsServices.updateContactById(id, req.body);
+  if (!result) throw HttpError(404, `Not found`);
+  res.json(result);
+};
+
+const deleteContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsServices.removeContactById(id);
+  if (!result) throw HttpError(404, `Not found`);
+  res.json(result);
+};
+
+const updateContactStatus = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsServices.updateContactStatusById(id, req.body);
+  if (!result) throw HttpError(404, `Not found`);
+  res.json(result);
+};
+
+export default {
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  deleteContact: ctrlWrapper(deleteContact),
+  addContact: ctrlWrapper(addContact),
+  updateContact: ctrlWrapper(updateContact),
+  updateContactStatus: ctrlWrapper(updateContactStatus),
+};
---
+++ services/contactsServices.js
@@ -0,0 +1,25 @@
+import Contact from '../models/Contact.js';
+
+const listContacts = () => Contact.find({}, '-createdAt -updatedAt');
+
+const addContact = data => Contact.create(data);
+
+const getContactById = id => {
+  const data = Contact.findById(id);
+  return data;
+};
+
+const updateContactById = (id, data) => Contact.findByIdAndUpdate(id, data);
+
+const removeContactById = id => Contact.findByIdAndDelete(id);
+
+const updateContactStatusById = (id, data) => Contact.findByIdAndUpdate(id, data);
+
+export default {
+  listContacts,
+  addContact,
+  getContactById,
+  updateContactById,
+  removeContactById,
+  updateContactStatusById,
+};
